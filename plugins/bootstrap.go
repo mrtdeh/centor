d48bdcddd7c60be0576f116dd6d894eb257a8d7a
mrtdeh <mrtdeh@gmail.com>
2023-12-03 14:44:49 +0330

    fix: clean code

--- plugins/bootstrap.go
+++ plugins/bootstrap.go
@@ -2,7 +2,6 @@ package pluginManager
 
 import (
 	PluginKits "github.com/mrtdeh/centor/plugins/assets"
-	echo_plugin "github.com/mrtdeh/centor/plugins/echo"
 	installer_plugin "github.com/mrtdeh/centor/plugins/installer"
 	system_plugin "github.com/mrtdeh/centor/plugins/system"
 	timeSyncer_plugin "github.com/mrtdeh/centor/plugins/time_syncer"
@@ -14,12 +13,6 @@ type Config struct {
 
 func Bootstrap(cnf Config) error {
 	return PluginKits.Loader(cnf.Config, func(pms *PluginKits.PluginManagerService) {
-
-		pms.AddPlugin(&echo_plugin.PluginProvider{
-			PluginProps: PluginKits.PluginProps{
-				Name: "echo",
-			},
-		})
 
 		pms.AddPlugin(&installer_plugin.PluginProvider{
 			PluginProps: PluginKits.PluginProps{
